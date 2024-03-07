853087b8142c7e2bd60e5720a90f8f9cd9302f03
Bhavesh Bhagtani <bbhagtani23@my.whitworth.edu>
2024-03-07 00:28:58 -0500

    message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -8,8 +8,20 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
+   <AppProvider>
+    <div className="App mx-5">
+        <h1>Shopping App</h1>
+        <div className = " d-flex gap-3 ">
+            <CartValue/>
+            <Location/>
+        </div>
+
+        <h2>Shopping Cart</h2>
+        <ExpenseList></ExpenseList>
+        <h2>Add Items</h2>
+        <ItemSelected></ItemSelected>
     </div>
+</AppProvider>
   );
 }
 
